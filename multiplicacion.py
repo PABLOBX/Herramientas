7d6f24f35759fa7206dc7f1ab8cd7b84c6687379
Pablo Bejarano Muñoz <pablobejarano8@javerianacali.edu.co>
2024-05-02 17:09:24 -0500

    multi

---
+++ multiplicacion.py
@@ -0,0 +1,5 @@
+def multiplicacion():
+    numero1 = int(input("introduzca el primer numero"))
+    numero2 = int(input("introduzca el primer numero"))
+    multiplicacion_final = numero1 * numero2
+    return multiplicacion_final
